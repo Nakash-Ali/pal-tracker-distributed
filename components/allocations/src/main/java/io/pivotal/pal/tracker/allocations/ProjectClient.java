5417386e0bf0e24fdf4fbe6992f12c8f60a42550
Vincent Chau <chau.vincent@gmail.com>
2019-11-07 14:30:59 -0500

    add circuit breaker

--- components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
@@ -1,18 +1,31 @@
 package io.pivotal.pal.tracker.allocations;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.client.RestOperations;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProjectClient {
 
     private final RestOperations restOperations;
-    private final String registrationServerEndpoint;
+    private final String endpoint;
+    private Map<Long, ProjectInfo> cachedProjects = new HashMap<>();
 
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
-        this.restOperations= restOperations;
-        this.registrationServerEndpoint = registrationServerEndpoint;
+        this.restOperations = restOperations;
+        this.endpoint = registrationServerEndpoint;
     }
 
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        ProjectInfo project =  restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+        cachedProjects.put(projectId, project);
+        return project;
     }
+
+    public ProjectInfo getProjectFromCache(long projectId) {
+        return cachedProjects.get(projectId);
+    }
+
 }
